2967e79dfdd98241cc70b16a43560d52966db2bf
Carlos Stenzel <carlosstenzel@hotmail.com>
2019-07-31 07:29:11 -0300

    arrays exemplos

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var arr = [1, 3, 4, 5, 6, 8, 9];
-var newArr = arr.map(function (item) {
-  return item * 2;
+var newArr = arr.map(function (item, index) {
+  return item * index;
 });
 console.log(newArr);
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const arr = [1, 3, 4, 5, 6, 8, 9];
 
-const newArr = arr.map(function(item) {
-    return item * 2;
+const newArr = arr.map(function(item, index) {
+    return item * index;
 });
 
 console.log(newArr);
