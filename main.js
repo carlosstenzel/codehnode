7fedf6299ae08a523a74d846c061497c32caec31
Carlos Stenzel <carlosstenzel@hotmail.com>
2019-07-31 07:34:44 -0300

    arrow function

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
 const arr = [1, 3, 4, 5, 6, 8, 9];
 
-const newArr = arr.map(function(item, index) {
-    return item * index;
+const newArr = arr.map((item) => {
+    return item * 2;
 });
 
 console.log(newArr);
 
-const sum = arr.reduce(function(total, next) {
-    return total + next;
-});
-
-console.log(sum);
-
-const filter = arr.filter(function(item) {
-    return item % 2 === 0;
-});
-
-console.log(filter);
-
-const find = arr.find(function(item) {
-    return item === 4;
-});
-
-console.log(find);
