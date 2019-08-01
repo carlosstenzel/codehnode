5767974cb87bf1f9db7ea873414eee89ae8fabfd
Carlos Stenzel <carlosstenzel@hotmail.com>
2019-08-01 07:22:29 -0300

    Promise teste

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-alert('Teste WebPack');
+const minhaPromise = () => new Promise((resolve, rejects) => {
+    setTimeout(() => { resolve('Ok') } , 2000);
+});
+
+minhaPromise().then(response => { 
+    console.log(response);
+});
