b42e5316c445389d479df57755a74465cc60f01a
Carlos Stenzel <carlosstenzel@hotmail.com>
2019-08-01 07:37:42 -0300

    async await

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ const minhaPromise = () => new Promise((resolve, rejects) => {
     setTimeout(() => { resolve('Ok') } , 2000);
 });
 
-minhaPromise().then(response => { 
-    console.log(response);
-});
+async function executaPromise() {
+    console.log( await minhaPromise() );
+    console.log( await minhaPromise() );
+
+    console.log( await minhaPromise() );
+
+}
+
+executaPromise();
